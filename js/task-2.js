1a492e4c9ceceb2028641fd3b5858afdcab74bac
RedisculuS <Altunina311@gmail.com>
2024-04-16 17:35:56 +0300

    1st commit
    before autocheck and mentor`s check

---
+++ js/task-2.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexOfRemovalItem = this.#items.indexOf(itemToRemove);
+    if (indexOfRemovalItem !== -1) {
+      this.#items.splice(0, indexOfRemovalItem);
+    }
+  }
+}
+
+console.log('Task 2');
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+
+// console.log(storage);
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
